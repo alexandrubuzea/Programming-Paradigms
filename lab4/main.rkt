03fab4bade3a9833d6750cd424b5b3c6ede9b7c4
alexandrubuzea <alexandru.buzea2007@stud.acs.upb.ro>
2023-04-06 14:21:11 +0300

    update/labs-4-and-5

---
+++ lab4/main.rkt
@@ -0,0 +1,126 @@
+#lang racket
+
+; scope al unei variabile = domeniul sau de vizibilitate = multimea liniilor / locurilor in care acea variabila este vizibila
+; context computational = o multime de perechi (simbol - valoare) care sunt caracteristice unui anumit loc din programul nostru
+
+; niste exemple utile
+
+; aici scope-ul variabilei x este DOAR in interiorul let-ului
+; contextul este nul inainte de let, apoi este {(x 2)} in interiorul let-ului, apoi este din nou nul (linia 12)
+(let ([x 2])
+  (+ x 2))
+
+; (* x 5) ; va da eroare de compilare: x este vizibil doar in interiorul let-ului.
+
+; in precursorul lui Racket numit Scheme (dar si in Lisp), `define` facea legare dinamica, de asemenea, puteam redefini simboluri
+; in Racket acest lucru nu mai este adevarat (legarea dinamica cu define ar putea introduce efecte laterale, ceea ce nu e permis
+; conform paradigmei functionale)
+
+(define a 10) ; define in limbajul Racket va face legare statica globala (NU legare dinamica)
+; (define a 30) -> in Scheme/Lisp era posibil, in Racket nu mai este
+
+; legare statica locala in limbajul Racket putem face cu:
+
+; 1. let
+
+; Sintaxa: (let ([e1 val1] [e2 val2] [e3 val3] ... [en valn]) expr)
+
+; In cazul unui let, scope-ul unei variabile este strict corpul let-ului (fara zonele de definire ale celorlalte variabile)
+
+; in urmatorul let, legarile variabilelor vor fi facute pe rand, astfel:
+; i. prima data, a se leaga la 5
+; ii. apoi, b se leaga la valoarea lui a din contextul superior ! (nu la a = 5, ci la a-ul din contextul global)
+; Deci b = 10
+; iii. la final, se va evalua expresia (+ 5 10) adica 15
+
+(let ([a 5] [b a])
+  (+ a b)
+  )
+
+; 2. let*
+
+; Sintaxa este aceeasi ca la let
+
+; Diferenta ? Scope-ul unei variabile incepe in momentul definitiei ei si se termina la finalul let-ului/expresiei.
+
+; Deci evaluarea urmatorului let* va decurge astfel:
+; i. a se leaga la 5
+; ii. b se leaga la valoarea lui a definita anterior (adica 5)
+; iii. Se evalueaza (+ 5 5) adica 10
+
+(let* ([a 5] [b a])
+  (+ a b)
+  )
+
+; 3. letrec
+
+; Sintaxa este aceeasi ca la let
+
+; Diferenta ? Scope-ul este, de aceasta data, tot letrec-ul (inclusiv inaintea zonei de definire).
+; Intrucat putem folosi oricare variabila in procesul de legare, putem avea recursivitate mutuala (a depinde de b, b depinde de a)?
+; Raspunsul este: depinde. Spre exemplu, urmatorul letrec va da eroare de compilare.
+
+(define b 20)
+
+; (letrec ([a b] [b a]) (+ a b))
+
+; Dar urmatoarea expresie este valida (functiile sunt remarcabile prin faptul ca continutul lor nu se evalueaza in momentul definirii
+; - inca nu am apelat functia)
+
+(letrec ([my-even (λ(x) (if (= x 0) #t (my-odd (- x 1))))]
+         [my-odd (λ(x) (if (= x 0) #f (my-even (- x 1))))]
+         ) (my-odd 5))
+
+; Practic, la momentul definirii, compilatorul nu va inspecta continutul functiilor. E necesar doar ca la runtime (aka momentul cand
+; facem (my-odd 5)) sa stie care este definitia lui my-even (amintim evaluarea intarziata a corpului functiilor)
+
+; 4. named let
+
+; Putem avea situatii cand avem nevoie de o functie helper locala, fara insa sa fie nevoie sa ne definim global acel helper (ex.
+; recursivitate pe coada)
+
+; In acest caz, putem sa ne definim o functie recursiva locala sub forma unui let, iar acest let sa ne preia numai parametrii
+; dupa care facem recursivitatea
+; Avantaj: named let va avea un context superior de unde poate prelua acei parametri ai functiei mari care nu se modifica, si
+; un context intern, in care are inclusiv variabilele dupa care se face recursivitatea).
+; Un exemplu concret:
+
+; Sa se scrie o functie care primeste o functie f, un numar n si calculeaza f^n (f compus cu f de n ori).
+; Evident, parametrul f nu se va modifica, de aceea facem urmatoarea functie (in forma curried):
+
+(define multicompose
+  (λ(f)
+    (λ(n)
+      (let helper-compose ([n n] [acc identity])
+        (if (= n 0) acc (helper-compose (- n 1) (compose acc f))))
+        )
+      )
+    )
+
+(((multicompose (curry * 2)) 10) 1) ; rezultatul va fi functia care inmulteste cu 2^10, aplicata pe 1 = 1024
+
+; ce este o inchidere functionala ?
+; in limbajul Racket, in momentul in care o lambda-expresie este definita (lambda-expresie = textul unei functii lambda),
+; are loc o inchidere functionala = functia lambda isi va salva contextul exterior in care ea a fost definita
+
+; exemplu concret:
+
+(define c 10)
+(define f (λ(x) (+ x c)))
+
+; aici expresia va fi evaluata la 10, intrucat functia va folosi valoarea lui c din momentul definirii ei
+; aka c = 10, nu c = 20 din contextul folosirii functiei
+(let ([c 20]) (f 10))
+
+; Exercitiu: ce se va afisa in urmatoarele exemple ?
+(define a1 1)
+(define a2 2)
+(define a3 3)
+
+(let ([a1 a2] [a2 a3] [a3 a1]) (+ a1 a2 a3))
+
+(let* ([a1 a2] [a2 a3] [a3 a1]) (+ a1 a2 a3))
+
+(let ([map filter] [filter map])
+  ((compose (curry map odd?) (curry filter (λ(x) (+ x (quotient x 2))))) '(1 2 3))
+  )
---
+++ lab5/main.rkt
@@ -0,0 +1,143 @@
+#lang racket
+(require racket/stream)
+
+; reamintim faptul ca Racket foloseste modelul de evaluare aplicativa
+; aka (+ 1 (+ 2 3)) se va evalua din interior spre exterior (+ exterior va cere evaluarea parametrilor sai, iar
+; evaluarea are loc astfel:
+; (+ 1 (+ 2 3)) -> (+ 1 5) -> 6
+
+; evaluarea se poate realiza insa si in alta ordine: din motive de performanta, noi am putea evalua o expresie
+; numai atunci cand avem nevoie explicit de ea.
+
+; avem deci nevoie de o metoda care sa ne realizeze intarzierea evaluarii pana in momentul in care avem nevoie
+; de rezultatul operatiei noastre
+; cunoastem deja o metoda in acest sens: inchiderea functionala.
+; ce facem insa daca putem evalua corpul si nu mai avem parametrii de dat ? -> folosim inchideri nulare :)
+; (aka lambda-uri cu 0 parametri)
+; Exemple:
+
+; chiar daca noi am putea evalua rezultatul, totusi nu o vom face: avem de "evaluat" functia lambda la momentul la care trebuie
+(define result (λ() (+ 2 3)))
+
+; ce facem insa daca vrem sa folosim valoarea lui result de mai multe ori ?
+; raspuns: inchiderea functionala este dummy (se va evalua de fiecare data cand este solicitata evaluarea ei)
+
+; Exemplu concret
+(define op (λ() (display "gigel\n") (+ 2 3)))
+(+ (op) (op))
+
+; deci daca avem de facut calcule complexe care trebuie intarziate, trebuie sa facem de fiecare data valoarea acelei inchideri.
+; avem alternativa ?
+; raspuns: DA. Avem un mecanism builtin in Racket, care se numeste promisiune.
+
+(define b (delay 5)) ; b aici va avea tipul #<promise>
+
+; ne salveaza promisiunile de dezavantajul de la inchideri de a evalua calculul de fiecare data ? Raspuns: DA
+(define c (delay (display "gigel2\n") (+ 2 3)))
+c
+(force c)
+c
+(force c)
+
+; acest nou tip de evaluare se numeste evaluare intarziata/lenesa. Haskell va folosi exclusiv evaluare lenesa din motive de performanta.
+; Cum putem realiza liste cu evaluare lenesa ? (liste care se evalueaza numai pe masura ce ne trebuie elemente din ele)
+
+; 1. Cu inchideri functionale
+(define (clojure-take s n)
+  (if (= n 0) null
+      (cons (car (s)) (clojure-take (cdr (s)) (- n 1)))
+  )
+  )
+
+(define clojure-ones (λ() (cons 1 clojure-ones)))
+
+(clojure-take clojure-ones 50)
+
+; 2. Folosind promisiuni
+(define (delay-take s n)
+  (if (= n 0) null
+      (cons (car s) (delay-take (force (cdr s)) (- n 1)))
+  )
+)
+
+(define delay-ones (cons 1 (delay delay-ones)))
+(delay-take delay-ones 10)
+
+; observati ca mai sus, datorita evaluarii lenese, am putut genera lista infinite. WOW !
+
+; din fericire, Racket ofera o interfata simplificata pentru lucrul cu streams.
+; avem functii precum stream-cons (cons de la liste), stream-first (car de la liste), stream-last (cdr de la liste) si
+; functionale precum stream-map, stream-filter. De asemenea, avem si stream-ul gol (empty-stream)
+
+; pentru inceput, pentru a vizualiza stream-urile, definim o functie care ne extrage primele n numere
+; dintr-un stream dat.
+
+(define (stream-take s n)
+  (cond
+    ((equal? s empty-stream) null)
+    ((= n 0) null)
+    (else (cons (stream-first s) (stream-take (stream-rest s) (- n 1))))
+    )
+  )
+
+; sa definim stream-uri din ce in ce mai interesante ...
+
+; A. stream-ul numerelor naturale (al tuturor)
+; pentru acest stream, avem mai multe variante de definire, astfel:
+; varianta 1: naturals in functie de ... el insusi
+(define naturals-1 (stream-cons 0 (stream-map (λ(x) (+ x 1)) naturals-1)))
+
+; varianta 2: naturals folosind un helper, care ne genereaza elementele de la o valoare k incolo, si particularizarea helper-ului pentru k = 0
+(define helper-naturals (λ(k) (stream-cons k (helper-naturals (+ k 1)))))
+
+(define naturals-2 (helper-naturals 0))
+
+; varianta 3: naturals folosind stream-ul de ones si o functie care imi combina 2 streams, numita stream-zip-with
+(define ones (stream-cons 1 ones))
+(define (stream-zip-with f s1 s2)
+  (cond
+    ((stream-empty? s1) s2)
+    ((stream-empty? s2) s1)
+    (else (stream-cons (f (stream-first s1) (stream-first s2)) (stream-zip-with f (stream-rest s1) (stream-rest s2))))
+    )
+  )
+
+(define naturals-3 (stream-cons 0 (stream-zip-with + ones naturals-3)))
+
+; B. stream-ul patratelor perfecte
+
+; varianta 1: putem mapa stream-ul numerelor naturale cu λ(x) (* x x) si obtinem patratele perfecte
+(define squares-1 (stream-map (λ(x) (* x x)) naturals-1))
+
+; varianta 2: putem filtra patratele perfecte din stream-ul numerelor naturale.
+(define is-square?
+  (λ(n)
+    (let helper-square ([d 0])
+      (cond
+        ((= d (+ n 1)) #f)
+        ((= n (* d d)) #t)
+        (else (helper-square (+ d 1)))
+        )
+      )
+    )
+  )
+
+(define squares-2 (stream-filter is-square? naturals-1))
+
+(stream-take squares-2 10)
+
+; C. stream-ul numerelor prime
+
+; pentru acest stream, vom folosi un algoritm cunoscut sub numele de ciurul lui Eratostene.
+; care sunt pasii ? -> luam primul element din stream si eliminam din restul stream-ului acele elemente divizibile cu el (care deci nu pot fi prime)
+; avem nevoie de o functie auxiliara care ne primeste un stream si ne aplica ciurul pe el.
+
+(define sieve
+  (λ(s) 
+    (let ([elem (stream-first s)])
+      (stream-cons elem (sieve (stream-filter (λ(x) (> (remainder x elem) 0))
+                             (stream-rest s)))))))
+
+(define primes (sieve (stream-rest (stream-rest naturals-3))))
+
+(stream-take primes 20)
